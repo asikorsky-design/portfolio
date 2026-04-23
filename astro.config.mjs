f74a258c9b7199914a7e7928f23e80873524c108
Alexander <alexander@MacBook-Pro-16--Alexander.local>
2026-04-23 23:33:06 +0300

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // your configuration options here...
+  vite: {
+    plugins: [tailwindcss()],
+
+    //исправляет ошибку https://github.com/withastro/astro/issues/15857
+    //optimizeDeps: {
+    //  exclude: ['astro/runtime/client/dev-toolbar/entrypoint.js'],
+    //}
+  },
+
+  integrations: [sitemap()],
+  //отключить панель инструментов разработчика
+  devToolbar: {
+    enabled: false
+  }
+
+});
